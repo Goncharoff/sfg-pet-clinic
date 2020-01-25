8a407f9e3cdcbe0082637d51af19abe7b8e66854
Nik <nik.ignition.work@gmail.com>
2020-01-25 22:42:06 +0200

    init commit

---
+++ src/main/java/com/goncharoff/sfgpetclinik/SfgPetClinikApplication.java
@@ -0,0 +1,13 @@
+package com.goncharoff.sfgpetclinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goncharoff/sfgpetclinik/SfgPetClinikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goncharoff.sfgpetclinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
