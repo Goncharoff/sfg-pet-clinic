11e74d09302f3189b8ef48143ef7d539a3d6cd01
Nik <nik.ignition.work@gmail.com>
2020-01-26 22:36:59 +0200

    added owners controller and view-page templates

--- pet-clinic-web/src/main/java/com/goncharoff/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/goncharoff/sfgpetclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package com.goncharoff.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "//index.html"})
-    public String listController() {
-        return "vets/index";
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOfOwners() {
+        return "owners/index";
     }
 }
