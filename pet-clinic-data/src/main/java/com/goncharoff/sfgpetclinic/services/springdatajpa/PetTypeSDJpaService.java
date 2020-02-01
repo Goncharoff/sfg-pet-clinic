8dc41456bf91be0ae13f4d71918ea93eb693d3e3
Nik <nik.ignition.work@gmail.com>
2020-02-02 01:52:28 +0200

    implemented pet type jpa service

---
+++ pet-clinic-data/src/main/java/com/goncharoff/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,52 @@
+package com.goncharoff.sfgpetclinic.services.springdatajpa;
+
+import com.goncharoff.sfgpetclinic.model.Pet;
+import com.goncharoff.sfgpetclinic.model.PetType;
+import com.goncharoff.sfgpetclinic.repositories.PetTypeRepository;
+import com.goncharoff.sfgpetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Can not find petType with id " + id));
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
