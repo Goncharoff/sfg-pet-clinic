d176411a9df47a89a18722bd8688ecf28b6335eb
Nik <nik.ignition.work@gmail.com>
2020-02-02 01:55:21 +0200

    implemented pet jpa service

---
+++ pet-clinic-data/src/main/java/com/goncharoff/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,50 @@
+package com.goncharoff.sfgpetclinic.services.springdatajpa;
+
+import com.goncharoff.sfgpetclinic.model.Pet;
+import com.goncharoff.sfgpetclinic.repositories.PetRepository;
+import com.goncharoff.sfgpetclinic.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Autowired
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElseThrow(() -> new RuntimeException("Can not find pet with id " + id));
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
